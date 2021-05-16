eca67e6afd45a432634426326a62b379349204bf
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-05-16 15:28:04 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="travelperk-http-python",
+    version="0.0.1",
+    description="Python SDK for accessing the TravelPerk API ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/namelivia/travelperk-http-python",
+    author="José Ignacio Amelivia Santiago",
+    author_email="jignacio.amelivia@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="travelperk, api",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.8, <4",
+    install_requires=["travelperk-python-api-types"],
+    project_urls={
+        "Bug Reports": "https://github.com/namelivia/travelperk-http-python/issues",
+    },
+)
