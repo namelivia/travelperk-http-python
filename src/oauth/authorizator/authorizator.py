35556d642e55dbda96548e088e6c661227229e5a
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-05-16 13:13:14 +0200

    Implement authorizator class

---
+++ src/oauth/authorizator/authorizator.py
@@ -0,0 +1,43 @@
+from urllib.parse import urlencode
+from typing import List
+from .scopes import Scopes
+from src.oauth.config.config import Config
+from src.oauth.constants.constants import AUTHORIZE_URL
+
+
+class Authorizator:
+    def __init__(
+        self,
+        config: Config,
+        # TODO: Token persistence
+        scopes: List[str],
+    ):
+        self.config = config
+        self.scopes = Scopes(scopes)
+
+    def get_auth_uri(self, target_link_uri: str) -> str:
+        return (
+            AUTHORIZE_URL
+            + "?"
+            + urlencode(
+                {
+                    "client_id": self.config.get_client_id(),
+                    "redirect_uri": self.config.get_redirect_url(),
+                    "scope": self.scopes.as_url_param(),
+                    "response_type": "code",
+                    # TODO: This should be more sofisticate to avoid vulnerabilities.
+                    # https://github.com/namelivia/travelperk-http-php/issues/21
+                    # base64 encoded info could be sent to have url, method and also a nonce.
+                    # More info here: https://tools.ietf.org/id/draft-bradley-oauth-jwt-encoded-state-08.html
+                    "state": target_link_uri,
+                }
+            )
+        )
+
+    def set_authorization_code(self, code: str) -> "Authorizator":
+        self.config.set_code(code)
+        return self
+
+    def is_authorized(self) -> bool:
+        # return self.token_persistence->has_token() or self.config.has_code()
+        return self.config.has_code()
--- tests/oauth/authorizator/test_authorizator.py
+++ tests/oauth/authorizator/test_authorizator.py
@@ -1,9 +1,45 @@
 import pytest
-from src.oauth.authorizator.scopes import Scopes
+from mock import Mock
+from src.oauth.authorizator.authorizator import Authorizator
+from src.oauth.config.config import Config
 
 
 class TestAuthorizator:
-    def test_building_with_invalid_scopes_raises_an_exception(self):
+    def setup(self):
+        self.config = Mock(spec=Config)
+        # TODO: Token persistence
+        self.scopes = ["expenses:read"]
+        self.authorizator = Authorizator(self.config, self.scopes)
+
+    def test_checking_authorized_no_code_on_config(self):
+        self.config.has_code.return_value = False
+        assert self.authorizator.is_authorized() is False
+        self.config.has_code.assert_called_once_with()
+
+    def test_checking_authorized_code_on_config(self):
+        self.config.has_code.return_value = True
+        assert self.authorizator.is_authorized() is True
+        self.config.has_code.assert_called_once_with()
+
+    def test_setting_the_authorization_code(self):
+        assert self.authorizator.set_authorization_code("auth-code")
+        self.config.set_code.assert_called_once_with("auth-code")
+
+    def test_getting_the_auth_uri(self):
+        self.config.get_client_id.return_value = "client-id"
+        self.config.get_redirect_url.return_value = "http://redirect.url"
+        assert (
+            self.authorizator.get_auth_uri("/target/link")
+            == "https://app.travelperk.com/oauth2/authorize/?client_id=client-id&redirect_uri=http%3A%2F%2Fredirect.url&scope=expenses%3Aread&response_type=code&state=%2Ftarget%2Flink"
+        )
+        self.config.get_client_id.assert_called_once_with()
+        self.config.get_redirect_url.assert_called_once_with()
+
+    def test_building_with_invalid_scopes_rise_exception(self):
         with pytest.raises(Exception) as e:
-            Scopes(["invalid:scope"])
+            Authorizator(
+                self.config,
+                # self.token_persistence,
+                self.scopes + ["invalid:scope"],
+            )
         assert str(e.value) == "The scope invalid:scope is invalid"
