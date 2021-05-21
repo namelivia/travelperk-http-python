d523e7ba5a601ebde95345a443fae087bb5829ad
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-05-21 09:21:08 +0200

    Fix client class

--- travelperk_http_python/client/client.py
+++ travelperk_http_python/client/client.py
@@ -1,17 +1,17 @@
 class Client:
-    def __init__(api_key: str):
+    def __init__(self, api_key: str):
         # TODO: Send  the following headers
         {
             "Api-Version": "1",
             "Authorization": "ApiKey " + api_key,
         }
 
-    def set_authorization_code(authorization_code: str) -> str:
+    def set_authorization_code(self, authorization_code: str) -> str:
         raise NotImplementedError(
             "No authorization code needed for simple api key authentication"
         )
 
-    def get_auth_uri(target_link_uri: str) -> str:
+    def get_auth_uri(self, target_link_uri: str) -> str:
         raise NotImplementedError(
             "No authorization URI for simple api key authentication"
         )
