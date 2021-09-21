cda4958edf8ea5e2878604affc1c917e3129a93f
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-09-21 10:49:07 +0200

    Yet another fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="travelperk-http-python",
-    version="1.0.6",
+    version="1.0.7",
     description="Python SDK for accessing the TravelPerk API ",
     long_description=long_description,
     long_description_content_type="text/markdown",
