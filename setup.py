07471c80d4686f157a339095f9b8c35a0eda027e
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-12-26 19:18:06 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="travelperk-http-python",
-    version="1.1.2",
+    version="1.2.0",
     description="Python SDK for accessing the TravelPerk API ",
     long_description=long_description,
     long_description_content_type="text/markdown",
